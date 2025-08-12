0fd8f4723a5753e9d1da8bf9ceaec8ca8b54c292
Prashant Dwivedi <prashantdwivedi247@gmail.com>
2025-08-13 00:07:05 +0530

    Lawyer Directory

---
+++ lawyer-directory/src/main/java/com/legal/lawyer_directory/model/UserLoginRequest.java
@@ -0,0 +1,25 @@
+package com.legal.lawyer_directory.model;
+
+public class UserLoginRequest {
+    private String email;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String password;
+    // getters and setters
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
